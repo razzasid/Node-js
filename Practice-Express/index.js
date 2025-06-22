a3e45856199ca3cf66c408929de4b79fadf892f8
Mohd Raza <razamohd6395@gmail.com>
2025-06-22 20:45:46 +0530

    learn some crud

--- Practice-Express/index.js
+++ Practice-Express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("node:fs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -8,7 +9,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + "public")));
 
 app.get("/", function (req, res) {
-  res.send("welcome");
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return;
+    }
+    // console.log("Files in directory:", files);
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`,"utf-8", (err, filedata) => {
+    res.render("show", {filename: req.params.filename , filedata: filedata})
+  });
+});
+
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000);
