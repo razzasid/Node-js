79cd532b1cd46448f36e3c480bd9b5b51a800467
Mohd Raza <razamohd6395@gmail.com>
2025-06-27 12:07:11 +0530

    learn some crud

--- Practice-Express/index.js
+++ Practice-Express/index.js
@@ -20,15 +20,29 @@ app.get("/", function (req, res) {
 });
 
 app.get("/file/:filename", function (req, res) {
-  fs.readFile(`./files/${req.params.filename}`,"utf-8", (err, filedata) => {
-    res.render("show", {filename: req.params.filename , filedata: filedata})
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", { filename: req.params.filename, filedata: filedata });
   });
+});
+
+app.get("/edit/:filename", function (req, res) {
+  res.render("edit", { filename: req.params.filename });
 });
 
 app.post("/create", function (req, res) {
   fs.writeFile(
     `./files/${req.body.title.split(" ").join("")}.txt`,
     req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.post("/edit", function (req, res) {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
     function (err) {
       res.redirect("/");
     }
