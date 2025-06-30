49d66445e60258a578120c195ad61fe83676c88a
Raza Siddique <razamohd6395@gmail.com>
2025-06-30 20:39:27 +0530

    minor change

--- CRUD-Operation/app.js
+++ CRUD-Operation/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const userModel = require("./models/user");
+const userModel = require("./models/userModel");
 
 app.set("view engine", "ejs");
 app.use(express.json());
--- CRUD-Operation/models/userModel.js
+++ CRUD-Operation/models/userModel.js
@@ -4,8 +4,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
 
 const userSchema = mongoose.Schema({
   image: String,
-  email: String,
+  email: { type: String, required: true, unique: true },
   name: String,
+  username: String,
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model("user", userSchema);
